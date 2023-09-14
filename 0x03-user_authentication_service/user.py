43b6bd72bcd025573857cac676fac392f66f095f
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2023-09-14 19:11:41 +0000

    user aunthentication service task

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-""" User module
+""" SQLAlchemy model for User
 """
-
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
@@ -9,20 +8,10 @@ Base = declarative_base()
 
 
 class User(Base):
-    """
-    User class.
-    """
-
+    """ User class """
     __tablename__ = 'users'
-
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
-
-    def __repr__(self):
-        """
-        String rep.
-        """
-        return f"User: id={self.id}"
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
